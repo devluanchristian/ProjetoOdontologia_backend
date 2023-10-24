e350b9f3e4ba6cb2878dec358de47fed65ea1560
Luan <luanchristian.rochadocarmo@gmail.com>
2023-10-23 23:29:49 -0300

    chore: :wrench: configurando server

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import fastify from 'fastify'
+import { ZodError } from 'zod'
+import { env } from './env'
+
+export const app = fastify()
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: 'Validation error',
+      issues: error.format(),
+    })
+  }
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // sda
+  }
+  return reply.status(500).send({ message: 'Internal server error' })
+})

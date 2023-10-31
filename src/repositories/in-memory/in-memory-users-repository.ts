98cb9fbe918ffdb547abb9a91e9b90b906388256
Luan <luanchristian.rochadocarmo@gmail.com>
2023-10-30 21:45:10 -0300

    feat: implementado funcionalidade no banco fake

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -3,12 +3,20 @@ import { IUsersRepository } from '../users-repository'
 
 export class InMemoryUsersRepository implements IUsersRepository {
   public items: User[] = []
+  async findbyEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+    if (!user) {
+      return null
+    }
+    return user
+  }
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
       id: 'user-01',
       RA: null,
-      users_TypesId: 'user-types',
+      date_create: new Date(),
+      last_date_acess: null,
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
